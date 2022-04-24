aa99903873224fe3bba4995bcaadf1c2634816c4
Matej Konrad <konrad.matej@gmail.com>
2022-04-24 14:03:09 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": ["eslint:recommended", "p5js", "p5js/sound"],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
